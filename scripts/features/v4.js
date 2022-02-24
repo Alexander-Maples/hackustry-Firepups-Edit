5df509cb11eb9011ad7cf70ad7db28460ec53eba
Alexander Maples <74429872+Alexander-Maples@users.noreply.github.com>
2022-02-24 09:57:04 -0600

    Delete to make override mod

--- scripts/features/v4.js
+++
@@ -1,8 +0,0 @@
-module.exports = (add) => {
-    
-    // launch to any sector
-    add("launch-anywhere", true, t => {
-        PlanetDialog.debugSelect = t;
-        Vars.content.sectors().each(e => e.alwaysUnlocked = t);
-    });
-};
